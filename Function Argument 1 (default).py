8907c06516f5b8c1887c3f2106867fbb5acfb191
joelroy08 <joelroypersonal@gmail.com>
2025-06-23 20:09:29 +0530

    Add files via upload

---
+++ Function Argument 1 (default).py
@@ -0,0 +1,10 @@
+# Default argument
+def joel(a,b,c=10,d=20):
+    print(a,b,c,d)
+
+joel(1,2,3,4) #Case 1
+
+joel(1,2)           #Case 2
+
+joel(1,2,5)      #Case 3
+
---
+++ Function arguments 2.py
@@ -0,0 +1,12 @@
+#Keyword arguments
+def personal_Details(Name,Age,salary,place):
+    salary=salary*2
+    print('Hi',Name)
+    print("your age is",Age)
+    print(salary*3)
+    print("your place is",place)
+
+personal_Details(Name='Joel Roy',
+                 Age=23,salary=300000,
+                 place='Koothattukulam') #Giving arguments using keys
+
