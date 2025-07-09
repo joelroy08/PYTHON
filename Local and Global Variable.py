683094afef945c5ebf1de2b315ae017fabd973cd
joelroy08 <joelroypersonal@gmail.com>
2025-07-09 19:26:05 +0530

    Add files via upload

---
+++ Local and Global Variable.py
@@ -0,0 +1,10 @@
+v=100             #Global variable
+
+def joel():
+    v=20          #Local variable
+    print(v)
+
+print(v)           #Output is Global variable
+
+
+joel()             #Output is local variable
