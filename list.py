22a78272fde31fb970152b7ebb380388bb491c74
joelroy08 <joelroypersonal@gmail.com>
2025-07-12 22:02:57 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+#Creation of a list
+joel=[12,34,5,75,3,46,75]
+#To find length
+print( len(joel))
+#To insert value to an index
+joel.insert(3,100)
+print(joel)
+#To remove the last element
+joel.pop()
+print(joel)
+#To print an index value
+print(joel[0])
