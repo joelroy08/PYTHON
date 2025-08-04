dc3ba7f4ad25ae080c68ba96a26285c770c798cb
Joel Roy <joelroypersonal@gmail.com>
2025-08-04 21:47:53 +0530

    Add files via upload

---
+++ Function odd or even.py
@@ -0,0 +1,13 @@
+def odd_num(num):
+    if num%2==0:
+     return False
+    else:
+      return True
+
+#print(odd_num(11))
+
+if odd_num(3):
+    print("The number is odd")
+
+else:
+    print("Its even number")
