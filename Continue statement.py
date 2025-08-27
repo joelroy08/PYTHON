787f837b41a324e969f4af3694a320e5e1f66336
Joel Roy <joelroypersonal@gmail.com>
2025-08-27 23:00:04 +0530

    Add files via upload

---
+++ Continue statement.py
@@ -0,0 +1,13 @@
+i=0           #initialisation
+while i<=10:  #Condition
+    i=i+1     #updation has to be placed
+              # here because otherwise the code
+              # will count upto 4 and then will
+              # automatically go to 5
+
+    if i==5:  #continue condition
+        continue
+    print(i, "hai")
+
+
+
